2215a0d61c8d621c6326fe8216eb30b92fa00e0f
Maynor Moe <maynormoe0@gmail.com>
2023-03-13 13:02:49 +0800

    update

--- src/components/SingerSingTop/SingerSingTop.vue
+++ src/components/SingerSingTop/SingerSingTop.vue
@@ -4,16 +4,34 @@
       <img alt="" src="/src/assets/img/defaultcover.png" />
     </div>
     <div v-if="singerTopData" class="rightList">
-      <el-table :data="singerTopData" stripe style="width: 100%">
-        <el-table-column label="排名" prop="index" width="180" />
-        <el-table-column label="歌名" prop="name" width="180" />
-        <el-table-column label="歌手" prop="ar[0].name" />
+      <el-table
+        :data="singerTopData.slice(0, 10)"
+        highlight-current-row
+        lazy
+        stripe
+        style="width: 100%"
+      >
+        <el-table-column type="index" width="50" />
+        <el-table-column prop="name" width="700" />
+        <el-table-column prop="ar[0].name" />
       </el-table>
+      <div class="showMore">
+        <span>查看全部</span>
+        <right
+          fill="#949494"
+          size="32"
+          strokeLinecap="butt"
+          strokeLinejoin="bevel"
+          theme="filled"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Right } from "@icon-park/vue-next";
+
 const props = defineProps({
   singerTopData: {
     type: Array,
@@ -40,8 +58,32 @@ const props = defineProps({
   }
 
   .rightList {
-    width: 60vmin;
-    height: 70vmin;
+    width: 100vmin;
+    height: 80vmin;
+
+    :deep(.el-table) {
+      border: 1px solid #ffffff;
+      border-radius: 3vmin;
+
+      .el-table__row {
+        border-radius: 3vmin;
+      }
+
+      .el-table__row--striped {
+        border-radius: 3vmin;
+      }
+    }
+
+    .showMore {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+
+      span {
+        text-align: left;
+        color: #949494;
+      }
+    }
   }
 }
 </style>
